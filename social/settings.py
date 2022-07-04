6826f98a47fa89545d724a16508b387edcb3ea1a
jsc-repo <108231974+jsc-repo@users.noreply.github.com>
2022-07-04 12:30:39 -0400

    cleaned up html files

--- social/settings.py
+++ social/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dwitter",
+    "crispy_forms",
+    "crispy_bulma",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ("bulma",)
+CRISPY_TEMPLATE_PACK = "bulma"
 
 LOGIN_REDIRECT_URL="dwitter:dashboard"
 LOGOUT_REDIRECT_URL="dwitter:dashboard"
